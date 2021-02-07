835fd16646c4b30bd6dc063ff87bd974b9150402
SergeyDorokhov <dorokhov-sergey@yandex.ru>
2021-02-07 21:52:15 +0300

    delete methods

--- test_add_group.py
+++ test_add_group.py
@@ -52,20 +52,6 @@ class AddGroup(unittest.TestCase):
     def open_home_page(self, wd):
         wd.get("http://localhost/addressbook/")
 
-    def is_element_present(self, how, what):
-        try:
-            self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e:
-            return False
-        return True
-
-    def is_alert_present(self):
-        try:
-            self.wd.switch_to_alert()
-        except NoAlertPresentException as e:
-            return False
-        return True
-
     def tearDown(self):
         self.wd.quit()
 
