c137cae8e05e1f380bfd8a52d683d83c5c0e118f
SergeyDorokhov <dorokhov-sergey@yandex.ru>
2021-02-07 21:51:19 +0300

    extract methods

--- test_add_group.py
+++ test_add_group.py
@@ -13,17 +13,17 @@ class AddGroup(unittest.TestCase):
 
     def test_add_group(self):
         wd = self.wd
-        # open home page
-        wd.get("http://localhost/addressbook/")
-        # login
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-        # init group creation
-        wd.find_element_by_link_text("groups").click()
+        self.open_home_page(wd)
+        self.login(wd)
+        self.open_groups_page(wd)
+        self.create_group(wd)
+        self.open_groups_page(wd)
+        self.logout(wd)
+
+    def logout(self, wd):
+        wd.find_element_by_link_text("Logout").click()
+
+    def create_group(self, wd):
         wd.find_element_by_xpath("(//input[@name='new'])[2]").click()
         # fill group form
         wd.find_element_by_name("group_name").click()
@@ -37,10 +37,20 @@ class AddGroup(unittest.TestCase):
         wd.find_element_by_name("group_footer").send_keys("333")
         # submit group creation
         wd.find_element_by_name("submit").click()
-        # return groups page
+
+    def open_groups_page(self, wd):
         wd.find_element_by_link_text("groups").click()
-        # logout
-        wd.find_element_by_link_text("Logout").click()
+
+    def login(self, wd):
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_xpath("//input[@value='Login']").click()
+
+    def open_home_page(self, wd):
+        wd.get("http://localhost/addressbook/")
 
     def is_element_present(self, how, what):
         try:
