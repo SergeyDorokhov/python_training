3b5a7cea646a994eba43d209e6db1d9de4574982
SergeyDorokhov <dorokhov-sergey@yandex.ru>
2021-02-08 21:59:45 +0300

    add test for create contact

---
+++ contact.py
@@ -0,0 +1,5 @@
+class Contact:
+    def __init__(self, firstname, lastname, home_phone):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.home_phone = home_phone
---
+++ test_add_contact.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+import unittest
+from contact import Contact
+
+from selenium import webdriver
+
+
+class TestAddContact(unittest.TestCase):
+    def setUp(self):
+        self.wd = webdriver.Firefox()
+        self.wd.implicitly_wait(30)
+
+    def test_add_contact(self):
+        wd = self.wd
+        self.open_auth_page(wd)
+        self.login(wd)
+        self.create_contact(wd, Contact(firstname="AAA", lastname="BBB", home_phone="123456789"))
+        self.open_home_page(wd)
+        self.logout(wd)
+
+    def open_home_page(self, wd):
+        wd.find_element_by_link_text("home").click()
+
+    def logout(self, wd):
+        wd.find_element_by_link_text("Logout").click()
+
+    def create_contact(self, wd, contact):
+        wd.find_element_by_link_text("add new").click()
+        # fill contact form
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.firstname)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact.lastname)
+        wd.find_element_by_name("home").click()
+        wd.find_element_by_name("home").clear()
+        wd.find_element_by_name("home").send_keys(contact.home_phone)
+        # submit contact creation
+        wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
+
+    def login(self, wd):
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_xpath("//input[@value='Login']").click()
+
+    def open_auth_page(self, wd):
+        wd.get("http://localhost/addressbook/")
+
+    def tearDown(self):
+        self.wd.quit()
+
+
+if __name__ == "__main__":
+    unittest.main()
