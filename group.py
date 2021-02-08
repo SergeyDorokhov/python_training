a1fa062fc5e9ae6c28b685e00f79060b2a33e114
SergeyDorokhov <dorokhov-sergey@yandex.ru>
2021-02-08 18:45:54 +0300

    add Group class

---
+++ group.py
@@ -0,0 +1,5 @@
+class Group:
+    def __init__(self, name, header, comment):
+        self.name = name
+        self.header = header
+        self.comment = comment
--- test_add_group.py
+++ test_add_group.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import unittest
+from group import Group
 
 from selenium import webdriver
 from selenium.common.exceptions import NoAlertPresentException
@@ -16,25 +17,25 @@ class AddGroup(unittest.TestCase):
         self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
         self.open_groups_page(wd)
-        self.create_group(wd, name="111", header="222", comment="333")
+        self.create_group(wd, Group(name="111", header="222", comment="333"))
         self.open_groups_page(wd)
         self.logout(wd)
 
     def logout(self, wd):
         wd.find_element_by_link_text("Logout").click()
 
-    def create_group(self, wd, name, header, comment):
+    def create_group(self, wd, group):
         wd.find_element_by_xpath("(//input[@name='new'])[2]").click()
         # fill group form
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(name)
+        wd.find_element_by_name("group_name").send_keys(group.name)
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(header)
+        wd.find_element_by_name("group_header").send_keys(group.header)
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(comment)
+        wd.find_element_by_name("group_footer").send_keys(group.comment)
         # submit group creation
         wd.find_element_by_name("submit").click()
 
