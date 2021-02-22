4674b0071ad6f491df9746bc4b801595d1002fae
SergeyDorokhov <dorokhov-sergey@yandex.ru>
2021-02-22 08:39:54 +0300

    Выделен метод поиска первой группы в списке групп, выделен метод заполнения полей формы редактирования группы

--- fixture/group.py
+++ fixture/group.py
@@ -6,33 +6,39 @@ class GroupHelper:
         wd = self.app.wd
         self.app.navigation.open_groups_page()
         wd.find_element_by_xpath("(//input[@name='new'])[2]").click()
-        self.fill_group_form(group, wd)
+        self.fill_group_form(group)
         wd.find_element_by_name("submit").click()
         self.app.navigation.open_groups_page()
 
     def delete_first_group(self):
         wd = self.app.wd
         self.app.navigation.open_groups_page()
-        wd.find_element_by_name("selected[]").click()
+        self.select_first_group()
         wd.find_element_by_name("delete").click()
         self.app.navigation.open_groups_page()
 
     def edit_first_group(self, group):
         wd = self.app.wd
         self.app.navigation.open_groups_page()
-        wd.find_element_by_name("selected[]").click()
+        self.select_first_group()
         wd.find_element_by_name("edit").click()
-        self.fill_group_form(group, wd)
+        self.fill_group_form(group)
         wd.find_element_by_name("update").click()
         self.app.navigation.open_groups_page()
 
-    def fill_group_form(self, group, wd):
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.comment)
+    def fill_group_form(self, group):
+        wd = self.app.wd
+        self.change_field_value("group_name", group.name)
+        self.change_field_value("group_header", group.header)
+        self.change_field_value("group_footer", group.comment)
+
+    def change_field_value(self, field_name, value):
+        wd = self.app.wd
+        if value is not None:
+            wd.find_element_by_name(field_name).click()
+            wd.find_element_by_name(field_name).clear()
+            wd.find_element_by_name(field_name).send_keys(value)
+
+    def select_first_group(self):
+        wd = self.app.wd
+        wd.find_element_by_name("selected[]").click()
--- model/group.py
+++ model/group.py
@@ -1,5 +1,5 @@
 class Group:
-    def __init__(self, name, header, comment):
+    def __init__(self, name=None, header=None, comment=None):
         self.name = name
         self.header = header
         self.comment = comment
