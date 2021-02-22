9eb717d631e112ae5ce6f70333a26983795741f3
SergeyDorokhov <dorokhov-sergey@yandex.ru>
2021-02-22 11:28:17 +0300

    Реализован интеллектуальный логин

--- tests/test_add_group.py
+++ tests/test_add_group.py
@@ -7,5 +7,10 @@ def test_add_group(app):
     app.group.create(Group(name="111", header="222", comment="333"))
 
 
+def test_add_group_with_logout(app):
+    app.group.create(Group(name="111", header="222", comment="333"))
+    app.session.logout()
+
+
 def test_add_empty_group(app):
     app.group.create(Group(name="", header="", comment=""))
