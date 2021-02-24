8544577975e0ae33620009ef0f79729d37636fdc
SergeyDorokhov <dorokhov-sergey@yandex.ru>
2021-02-24 11:55:50 +0300

    При переходе между страницами проверяется текущая страница приложения

--- fixture/navigation.py
+++ fixture/navigation.py
@@ -4,12 +4,18 @@ class NavigationHelper:
 
     def open_auth_page(self):
         wd = self.app.wd
-        wd.get("http://localhost/addressbook/")
+        if not (wd.current_url.endswith("addressbook/") and len(
+                wd.find_elements_by_xpath("//input[@value='Login']")) > 0):
+            wd.get("http://localhost/addressbook/")
 
     def open_home_page(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("home").click()
+        if not (wd.current_url.endswith("addressbook/") and len(
+                wd.find_elements_by_link_text("Number of results")) > 0):
+            wd.find_element_by_link_text("home").click()
 
     def open_groups_page(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("groups").click()
+        if not (wd.current_url.endswith("group.php") and len(
+                wd.find_elements_by_link_text("new")) > 1):
+            wd.find_element_by_link_text("groups").click()
