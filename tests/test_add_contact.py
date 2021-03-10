885659b9d9677b69d5dd52297f5fde8640c8e056
SergeyDorokhov <dorokhov-sergey@yandex.ru>
2021-03-10 09:50:50 +0300

    Параметризация теста на создание контактов

--- tests/test_add_contact.py
+++ tests/test_add_contact.py
@@ -1,12 +1,36 @@
 # -*- coding: utf-8 -*-
+import random
+import re
+import string
+
+import pytest
 
 from model.contact import Contact
 
 
-def test_add_contact(app):
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + " " * 5
+    rand_string = (prefix + " ".join([random.choice(symbols) for x in range(random.randrange(maxlen))]))
+    return re.sub(r'\s+', ' ', rand_string)
+
+
+def random_numbers(maxlen):
+    symbols = string.digits
+    rand_num = ("".join([random.choice(symbols) for x in range(random.randrange(maxlen))]))
+    return rand_num
+
+
+testdata = [
+    Contact(firstname=random_string("firstname: ", 10).rstrip(), lastname=random_string("lastname: ", 8).rstrip(),
+            home_phone=random_numbers(10), work_phone=random_numbers(10),
+            mobile_phone=random_numbers(7)) for
+    i in range(3)]
+
+
+@pytest.mark.parametrize("contact", testdata, ids=(repr(x) for x in testdata))
+def test_add_contact(app, contact):
     list_before = app.contact.get_list()
-    contact = Contact(firstname="AAA", lastname="BBB", id_contact=app.contact.get_next_id(list_before),
-                      home_phone="123456789", work_phone="456", mobile_phone="789")
+    contact.id_contact = app.contact.get_next_id(list_before)
     app.contact.create(contact)
     assert len(list_before) + 1 == app.contact.count()
     list_after = app.contact.get_list()
