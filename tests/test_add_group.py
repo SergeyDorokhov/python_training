52a406c8d4d8bcce49a83d2efbab7ee6e160dd00
SergeyDorokhov <dorokhov-sergey@yandex.ru>
2021-04-11 17:29:34 +0300

    Allure отчеты с расписанными шагами

--- tests/test_add_group.py
+++ tests/test_add_group.py
@@ -1,14 +1,18 @@
 # -*- coding: utf-8 -*-
+import allure
 
 from model.group import Group
 
 
 def test_add_group(app, db, json_groups, check_ui):
     group = json_groups
-    list_before = db.get_group_list()
-    app.group.create(group)
-    list_after = db.get_group_list()
-    list_before.append(group)
-    assert sorted(list_before, key=Group.id_or_max) == sorted(list_after, key=Group.id_or_max)
-    if check_ui:
-        assert sorted(list_after, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
+    with allure.step('Given a group list'):
+        list_before = db.get_group_list()
+    with allure.step('When i add a group %s to the list' % group):
+        app.group.create(group)
+    with allure.step('Then a new group list is equal the old list with added group'):
+        list_after = db.get_group_list()
+        list_before.append(group)
+        assert sorted(list_before, key=Group.id_or_max) == sorted(list_after, key=Group.id_or_max)
+        if check_ui:
+            assert sorted(list_after, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
